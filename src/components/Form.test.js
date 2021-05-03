0688d1ebad29ac8b90501ee9da0cac3f1dc756b6
ninel <zabaykinanm@mer.ci.nsu.ru>
2021-05-03 14:46:02 +0700

    fixed old tests

--- src/components/Form.test.js
+++ src/components/Form.test.js
@@ -1,15 +1,33 @@
+import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import Form from './Form';
-import React from 'react';
 
-test('can enter text and submit', () => {
+test('Checked form', () => {
+  const text = '123';
   const handleSubmit = jest.fn();
-  const field = 'field contents';
+
   render(<Form handleSubmit={handleSubmit} />);
-  const input = screen.getByTestId('input');
-  const form = screen.getByTestId('form');
-  fireEvent.input(input, { target: { value: field } }); // ввод в поле
+
+  fireEvent.input(screen.getByTestId('input'), {
+    target: {
+      value: text
+    }
+  });
+
   expect(handleSubmit).not.toBeCalled();
-  fireEvent.submit(form); // отправка формы
-  expect(handleSubmit).toBeCalledWith(expect.objectContaining({ field }));
+  fireEvent.submit(screen.getByTestId('form'));
+  expect(handleSubmit).toBeCalledWith(text);
+  expect(screen.getByTestId('input')).toHaveValue('');
 });
+
+test('Checked validation', () => {
+  const value = '';
+  const handleSubmit = jest.fn();
+  render(<Form handleSubmit={handleSubmit}/>);
+  const input = screen.getByTestId('input');
+  fireEvent.input(input, {
+    target: {
+      value: value
+    }
+  });
+})
--- src/components/ListItem.test.js
+++ src/components/ListItem.test.js
@@ -1,36 +1,42 @@
+import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import ListItem from './ListItem';
+import { ACTION_TYPES} from "../Store";
+
+const id = 123;
+const text = 'random text';
 
 test('ListItem displays content and reacts to the button', () => {
-  const id = 123;
-  const text = 'random text';
-  const deleteHandler = jest.fn();
+  const id = '321'
+  const text = 'text'
+  const dispatch = jest.fn();
 
-  render(<ListItem text={text} id={id} deleteHandler={deleteHandler} />);
-  expect(screen.getByText(text)).toBeInTheDocument();
+  render(<ListItem text={text} id={id} dispatch={dispatch} />);
+  const submitButton = screen.queryByText('submit')
+  expect(submitButton).toBeNull() // it doesn't exist
 
   const button = screen.getByTestId('delete_button');
   expect(button).toBeInTheDocument();
   fireEvent.click(button);
 
-  expect(deleteHandler).lastCalledWith(id);
+  expect(dispatch).toBeCalledWith({payload: id, type: ACTION_TYPES.DELETE});
 });
-test('List Item display selected checkbox ', () => {
-  const index = '1';
-  const item = 'Забыть спросить хохла';
-  render(<ListItem index={index} isChecked={true} item={item} />)
 
-  const checker = screen.getByTestId('check');
-  expect(checker).toBeInTheDocument();
-  expect(checker).toHaveAttribute('checked');
-})
+test('Checked checkbox mark', () => {
+  render(<ListItem id={id} text={text} isChecked={true} />);
+  const checkbox = screen.getByTestId('checkbox');
+  expect(checkbox).toBeInTheDocument();
+  expect(checkbox).toHaveAttribute('checked');
+});
 
-test('List Item display empty checkbox ', () => {
-  const index = '1';
-  const item = 'Забыть спросить хохла';
-  render(<ListItem index={index} isChecked={false} item={item} />)
+test ('Checked checkbox empty', () => {
+  render(<ListItem id={id} text={text} isChecked={false} />);
+  const checkbox = screen.getByTestId('checkbox');
+  expect(checkbox).toBeInTheDocument();
+  expect(checkbox).not.toHaveAttribute('checked');
+});
 
-  const checker = screen.getByTestId('check');
-  expect(checker).toBeInTheDocument();
-  expect(checker).not.toHaveAttribute('checked');
-})
+
+
+
+
